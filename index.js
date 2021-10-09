59e0b6f46f05690ff561fbe23dcc77246208b1b7
nemo-omen <nemo.omen@gmail.com>
2021-10-09 14:40:51 -0500

    Done-ish

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
-import { selectAll, select } from 'hast-util-select';
-import { parseSelector } from 'hast-util-parse-selector';
 import { visit } from 'unist-util-visit';
 
 
+/*
+options: 
+{
+ className: 'code-figure', 
+ captionOptions: {
+   disable: true | false, 
+   position: 'after' | 'before', 
+   className: 'code-caption'
+ }
+}
+*/
 
-// options: 
-// {
-//  className: 'code-figure', 
-//  captionOptions: {
-//    disable: true | false, 
-//    position: 'after' | 'before', 
-//    className: 'code-caption'
-//  }
-// }
 export default function codeFigure(
 options = {
     className:'code-figure', 
@@ -52,53 +51,8 @@ ${captionOptions.position === 'before' ? captionElement.value : ''}
 ${preElement.value}
 ${captionOptions.position === 'after' ? captionElement.value : ''}
 </figure>`
-      };
-      parent.children[index] = figElement;
-    });
-    
-  //   for(const preNode of selectAll('pre', tree)) {
-  //     const codeNode = select('code', preNode);
-  //     captionValue = extractCaptionValue(codeNode);
-  //     let captionElement = null;
-
-  //     visit(tree, preNode, (node, i, parent) => {
-
-        // if(captionValue !== null) {
-        //   captionElement = {
-        //     type: 'element',
-        //     tagName: 'figcaption',
-        //     properties: {
-        //       className: captionOptions.className ? captionOptions.className : 'code-caption',
-        //     },
-        //     children: [
-        //       { type: 'text', value: captionValue },
-        //     ]
-        //   };
-        // }
-
-  //       const figElement = {
-  //         type: 'element',
-  //         tagName: 'figure',
-  //         properties: {
-  //           className: className ? className : 'code-figure'
-  //         },
-  //         children: captionOptions.disable ? [preNode]
-  //           : captionOptions.position === 'before' ? [captionElement, preNode]
-  //           : [preNode, captionElement]
-  //       }
-  //       parent.children[i] = figElement;
-  //     });
-  //   }
-  // }
-    }
+    };
+    parent.children[index] = figElement;
+  });
   }
-
-function extractCaptionValue(codeNode) {
-  let meta = null;
-  
-  if(codeNode.data) {
-    meta =  codeNode.data.meta;
-  }
-  
-  return meta;
 }
